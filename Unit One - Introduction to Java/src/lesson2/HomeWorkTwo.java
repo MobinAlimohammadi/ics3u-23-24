11ac1856f99307842dcb72bb7aa8fc571535355d
Mobin Alimohammadi <malimohammadi@bayviewglen.ca>
2023-09-13 10:28:29 -0400

    Second hw task

--- Unit One - Introduction to Java/src/lesson2/HomeWorkTwo.java
+++ Unit One - Introduction to Java/src/lesson2/HomeWorkTwo.java
@@ -1,5 +1,16 @@
 package lesson2;
 
 public class HomeWorkTwo {
-    
+    public static void main(String[] args) {
+        System.out.print("Presentation"); /*this way the title can be interchanged by assiging a string variable a value and using it instead*/
+        System.out.println("Exploring Huron-Wendat Heritage");
+        System.out.println("aAspects of Huron-Wendat culture: \"Traditional Practices:");
+        System.out.println("Traditional Practices");
+        System.out.println( "Art and Craftsmanship,");
+        System.out.println(" Language and Storytelling,");
+        System.out.println("Community Traditions.");
+        System.out.println("Date: Wednesday, November 15, 2023");
+        System.out.println("Duration of your presentation: 60 minutes");
+
+    }
 }
